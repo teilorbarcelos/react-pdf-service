398f019e83bf25dfde16292125625854ab33ad3a
Teilor Souza Barcelos <tsb-@live.com>
2026-05-24 21:53:25 -0300

    feat: User list example pdf export test done

--- src/templates/index.ts
+++ src/templates/index.ts
@@ -1,6 +1,8 @@
 import React from 'react';
 import { SimpleTemplate } from './SimpleTemplate.js';
+import { UserListTemplate } from './UserListTemplate.js';
 
 export const templates: Record<string, React.ElementType> = {
   'simple': SimpleTemplate,
+  'user-list': UserListTemplate,
 };
